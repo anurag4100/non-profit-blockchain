a6250a7bad5712e4ef7f6b1ec2c34b8b0059d024
anurag4100 <anuragbharatpandey@gmail.com>
2019-09-05 08:03:47 +0530

    checking whether this works

--- ngo-rest-api/app.js
+++ ngo-rest-api/app.js
@@ -479,7 +479,7 @@ app.get('/height', awaitHandler(async (req, res) => {
 	let message = shell.exec('docker exec -e "CORE_PEER_TLS_ENABLED=true" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/home/managedblockchain-tls-chain.pem" \\\n' +
 		'    -e "CORE_PEER_ADDRESS=$PEER" -e "CORE_PEER_LOCALMSPID=$MSP" -e "CORE_PEER_MSPCONFIGPATH=$MSP_PATH" \\\n' +
 		'    cli peer channel getinfo -c mychannel', {silent:false}).stdout;
-	res.send(message);
+	res.send('{'+message+'}');
 }));
 /************************************************************************************
  * NGO methods
