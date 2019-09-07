8db1955ef831e284db98a5d5e4f79ccacae8e1a1
anurag4100 <anuragbharatpandey@gmail.com>
2019-09-07 12:34:29 +0530

    temp

--- ngo-chaincode/src/ngo.js
+++ ngo-chaincode/src/ngo.js
@@ -958,7 +958,7 @@ let Chaincode = class {
 	//let allContributionsA = await stub.getState(queryString2);
 	let allContributionsA = await queryByString(stub, queryString2);
 	//throw new Error("This is how the contributions is looking: "+JSON.stringify(allContributionsA.toString()));
-    let allContributions = JSON.stringify(allContributionsA.toString()));
+    let allContributions = JSON.stringify(allContributionsA.toString());
     let totalBalance = 0;
     for (let n = 0; n < allContributions.length; n++) {
       for (let m=0; m< allContributions[n]['Record']['investments'].length;m++){
