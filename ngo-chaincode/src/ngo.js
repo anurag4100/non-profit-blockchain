3636a75ced87a259c22cb8f7318b0e97d82cc50a
anurag4100 <anuragbharatpandey@gmail.com>
2019-09-07 13:11:29 +0530

    temp

--- ngo-chaincode/src/ngo.js
+++ ngo-chaincode/src/ngo.js
@@ -961,8 +961,8 @@ let Chaincode = class {
     let allContributions = JSON.stringify(allContributionsA.toString());
     let buf = Buffer.from(JSON.stringify(allContributionsA));
     let temp = JSON.parse(buf.toString());
-
-    throw new Error("This is how the Record is looking: "+JSON.stringify(temp));
+    console.error("error trying");
+    throw new Error("## all contrib: "+allContributions);
     let totalBalance = 0;
     for (let n = 0; n < allContributions.length; n++) {
       for (let m=0; m< allContributions[n]['Record']['investments'].length;m++){
